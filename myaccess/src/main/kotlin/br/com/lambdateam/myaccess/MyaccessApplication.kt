efa01d5e00abfdf0b7a3ecdb68f42d3fac66a6ea
cskmacedo <kleber.barreto@msn.com>
2022-06-08 02:59:59 -0300

    API de Usuários finalizada

--- myaccess/src/main/kotlin/br/com/lambdateam/myaccess/MyaccessApplication.kt
+++ myaccess/src/main/kotlin/br/com/lambdateam/myaccess/MyaccessApplication.kt
@@ -2,10 +2,17 @@ package br.com.lambdateam.myaccess
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
 class MyaccessApplication
 
 fun main(args: Array<String>) {
 	runApplication<MyaccessApplication>(*args)
+}
+
+@PostConstruct
+fun init() {
+	TimeZone.setDefault(TimeZone.getTimeZone("UTC-3:00"))
 }
